5dcdf7bb9a057f1baeb77811fd3bade3e4fb86f0
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-05-06 11:54:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
 import motor.motor_asyncio
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://user:user@cluster0.6hrmdnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client.get_database('librarymanagement')
 student_collection = db.get_collection('students')
